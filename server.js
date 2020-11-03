25579a939512b97988f316a4e8b41f0dd0663932
Peter Pan <petarzivkovic85@gmail.com>
2020-11-02 18:27:10 -0800

    server folder is completed

--- server.js
+++ server.js
@@ -14,3 +14,30 @@ app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout_slackerdb',
+ { useNewUrlParser: true, useUnifiedTopology: true });
+ mongoose.connection
+  .once('open', () => console.log('Good to go!'))
+  .on('error', (error) => {
+      console.warn('Error', error);
+  });
+
+db.Workouts.create({name: "Ultimate Workout Plan"})
+  .then(dbWorkouts => {
+    console.log('log from inside dbWorkouts.create '+dbWorkouts);
+  })
+  .catch( ({message}) => {
+    console.log('this is the error:' +message);
+  });
+
+  app.get("/", (req,res) =>{
+    console.log('GET root route');
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  })
+  app.use(require("./controllers/exercises"));
+  app.use(require("./controllers/workout"));
+  
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
